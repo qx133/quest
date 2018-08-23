5a31c5000a657dda7367def9c421f7527148f2e8
roshanraka <roshan7raka@gmail.com>
2018-08-23 12:25:48 +0530

    Delete serve.js

--- serve.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-//setting middleware
-app.use(express.static(__dirname+'/public')); //Serves resources from public folder
-
-var server = app.listen(5000);
-
-console.log('http://localhost:5000');
